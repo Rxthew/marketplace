fe921d0a2f697f1e20a3b678045d1d1145f0a382
rxthew <richardtheumaster@gmail.com>
2022-07-25 11:14:10 +0200

    Convert itemsArray to itemsMap and adjust

--- src/Pages/Cart.tsx
+++ src/Pages/Cart.tsx
@@ -7,8 +7,8 @@ interface cartItem {
 }
 
 interface cartProps {
-    itemsArray: cartItem[] | []
-    readonly itemsSetter: React.Dispatch<SetStateAction<cartItem[] |[]>>
+    itemsMap: Map<string,cartItem> | null
+    readonly itemsSetter: React.Dispatch<SetStateAction<Map<string,cartItem> | null>>
     
 
 }
@@ -17,40 +17,58 @@ interface cartProps {
 
 const Cart = function(props:cartProps):JSX.Element{
 
-    const incrementItem = function(){
+
+    const incrementItem = function(key:string){
 
     }
     
-    const decrementItem = function(){
+    const decrementItem = function(key:string){
     
     }
     
-    const removeItem = function(){
+    const removeItem = function(key:string){
         
     }
 
-    const renderItem = function(itemObject:cartItem){
+    const renderItem = function(key:string,itemObject:cartItem ){
         let amount = itemObject.itemAmount.toString()
 
         return (
             <div>
                 {itemObject.item}
-                <button>Decrement</button>
-                <span>{amount}</span>
-                <button>Increment</button>
+                <div>
+                    <button onClick = {() => {decrementItem(key)}}>Decrement</button>
+                    <span>{amount}</span>
+                    <button onClick={() => {incrementItem(key)}}>Increment</button>
+                </div>
+                <button onClick={() => {removeItem(key)}}>Remove</button>
+
             </div>
             
         )
     }
+    if(props.itemsMap){
+        const itemsArray =  Array.from(props.itemsMap.entries())
+        const renderedItems = itemsArray.map(elem => renderItem(elem[0],elem[1]))
+        return(
+            <div>
+                {renderedItems}
+            </div>
+        )
+        
+    }
 
-    const renderedItems = props.itemsArray.map(itemObj => renderItem(itemObj))
+    else{
+        return (
+            <div>
+                <span>Your cart is empty</span>
+            </div>
+        )
+    }
+    
 
 
-    return(
-        <div>
-            {renderedItems}
-        </div>
-    )
+    
 
 }
 
