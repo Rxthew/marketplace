e7626ddedace552d2b888e2c947482f0b057a22d
rxthew <richardtheumaster@gmail.com>
2022-07-26 15:25:13 +0200

    Refactor tests
    Context: Inappropriate use of jest.fn

--- src/Pages/tests/Cart.test.tsx
+++ src/Pages/tests/Cart.test.tsx
@@ -1,20 +1,20 @@
 import {render, screen} from '@testing-library/react'
-import { useState } from 'react'
+import {useState } from 'react'
 import userEvent from '@testing-library/user-event'
 import {Cart, cartItem} from '../Cart'
 
 
  
 describe('Cart renders props, including itemsMap, correctly', () => { 
-    const setNewMap = jest.fn(()=> {
-        let [propMap, setPropMap] = useState<Map<string,cartItem>| null>(new Map().set('default',{item: <div></div>, itemAmount: 1, itemPrice: 4}))
-        return {
-            mapCheck:propMap,
-            mapSet: setPropMap
-        }
-    })
+    const SetNewMap = function(props:{someMap : Map<string,cartItem> | null}){
+        let [propMap, setPropMap] = useState<Map<string,cartItem>| null>(props.someMap)
+        return (
+            <Cart itemsMap={propMap} itemsSetter={setPropMap}/>
+        )
+       
+    }
     
-    const someItem = jest.fn((text:string) =>{
+    const someItem = (text:string) =>{
         return(
             {
                 item : <div>{text}</div>,
@@ -23,34 +23,31 @@ describe('Cart renders props, including itemsMap, correctly', () => {
             }
             
         )
-    })
+    }
 
     it('Cart renders default empty message if null passed in as prop',() => {
-        const setMap = setNewMap().mapSet
-        render(<Cart itemsMap={null} itemsSetter={setMap}/>)
+        render(<SetNewMap someMap={null} />)
         const target = screen.getByText('Your cart is empty')
         expect(target).toBeInTheDocument()
 
     })
     it('Cart renders first item from Map',() => {
-        const firstItemMap = setNewMap()
-        if(!firstItemMap.mapCheck){return}
-        firstItemMap.mapCheck.set('default', someItem('first item')) 
-        render(<Cart itemsMap={firstItemMap.mapCheck} itemsSetter={firstItemMap.mapSet} />)
+        const firstItemMap = new Map()
+        firstItemMap.set('default',someItem('first item'))
+        render(<SetNewMap someMap={firstItemMap} />)
         const target = screen.getByText('first item')
         expect(target).toBeInTheDocument()
-        const emptyCart = screen.getByText('Your cart is empty')
-        expect(emptyCart).toBeNull()
+        expect(screen.queryByText('Your cart is empty')).not.toBeInTheDocument()
 
     })
 
     it('Cart renders multiple items from Map',() => {
-        const severalItemsMap = setNewMap()
-        if(!severalItemsMap.mapCheck){return}
-        severalItemsMap.mapCheck.set('default',someItem('another first item'))
-        severalItemsMap.mapCheck.set('a',someItem('second'))
-        severalItemsMap.mapCheck.set('b',someItem('third'))
-        render(<Cart itemsMap={severalItemsMap.mapCheck} itemsSetter={severalItemsMap.mapSet}/>)
+        const severalItemsMap = new Map()
+        severalItemsMap.set('default',someItem('another first item'))
+        severalItemsMap.set('a',someItem('second'))
+        severalItemsMap.set('b',someItem('third'))
+        
+        render(<SetNewMap someMap={severalItemsMap}/>)
         
         const target = screen.getByText('another first item')
         const second = screen.getByText('second')
@@ -62,18 +59,21 @@ describe('Cart renders props, including itemsMap, correctly', () => {
     })
 
     it('On unmount, card checks if there are any items with 0 as their item value and removes them accordingly', ()=>{
-        const itemsWith0Map = setNewMap()
-        if(!itemsWith0Map.mapCheck){return}
-        itemsWith0Map.mapCheck.set('default',Object.assign({},itemsWith0Map.mapCheck.get('default'),{itemAmount: 0}))
-        itemsWith0Map.mapCheck.set('present',someItem('present'))
+        const itemsWith0Map = new Map()
+        itemsWith0Map.set('default',Object.assign({},someItem('past'),{itemAmount: 0}))
+        itemsWith0Map.set('present',someItem('present'))
 
-        const {container,unmount} = render(<Cart itemsMap={itemsWith0Map.mapCheck} itemsSetter={itemsWith0Map.mapSet} />)
+        let {container,unmount} = render(<SetNewMap someMap={itemsWith0Map} />)
+        expect(screen.getByText('past')).toBeInTheDocument()
+        expect(screen.getByText('present')).toBeInTheDocument()
         unmount()
-        expect(itemsWith0Map.mapCheck.get('default')).toBeUndefined()
-        expect(itemsWith0Map.mapSet).toBeCalled()
-        expect(container.textContent).toContain('present')
+        expect(itemsWith0Map.get('default')).toBeUndefined()
+        expect(itemsWith0Map.get('present')).toBeTruthy()
+        render(<SetNewMap someMap={itemsWith0Map} />)
+        expect(screen.queryByText('past')).not.toBeInTheDocument()
+        expect(screen.getByText('present')).toBeInTheDocument()
     })
-    
+
     
 })
 
