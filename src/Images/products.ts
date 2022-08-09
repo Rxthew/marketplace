ad87a0031a84cb26e0d01e6be3338e8aa26eed97
rxthew <richardtheumaster@gmail.com>
2022-08-09 15:52:22 +0200

    Initialise default products module

---
+++ src/Images/products.ts
@@ -0,0 +1,21 @@
+import camera from './black-white-camera.jpg'
+import reel from './fishing-reel.jpg'
+import drive from './hard-drive.jpg'
+import kettle from './kettle.jpg'
+import bike from '/mountain-bike.jpg'
+import perfume from './perfume.jpg'
+import toolbox from './toolbox.jpg'
+import basket from './wicker-basket.jpg'
+
+const products = {
+    camera,
+    reel,
+    drive,
+    kettle,
+    bike,
+    perfume,
+    toolbox,
+    basket
+}
+
+export default products
